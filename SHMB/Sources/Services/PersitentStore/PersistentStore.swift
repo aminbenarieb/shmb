b57aa3224483bb171954a6296ceb515d8a781b81
Amin Benarieb <amin.benarieb.work@gmail.com>
2021-03-27 20:42:43 +0300

    Wed Mar 24 11:51:43 MSK 2021

--- SHMB/Sources/Services/PersitentStore/PersistentStore.swift
+++ SHMB/Sources/Services/PersitentStore/PersistentStore.swift
@@ -1,9 +1,24 @@
 import Combine
 import Foundation
 
-struct PersistentStoreStocksInfo: Codable {
+class PersistentStoreStocksInfo: NSObject, Codable, NSCoding {
     let id: String
     let favourite: Bool
+
+    init(id: String, favourite: Bool) {
+        self.id = id
+        self.favourite = favourite
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        self.id = aDecoder.decodeObject(forKey: "id") as! String
+        self.favourite = aDecoder.decodeBool(forKey: "favourite")
+    }
+
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.id, forKey: "id")
+        aCoder.encode(self.favourite, forKey: "favourite")
+    }
 }
 
 protocol PersistentFavouritesStore {
--- SHMB/Sources/Services/PersitentStore/PersistentStoreUserDefaultsImpl.swift
+++ SHMB/Sources/Services/PersitentStore/PersistentStoreUserDefaultsImpl.swift
@@ -1,10 +1,6 @@
 import Combine
 import Foundation
 
-enum PersistentStoreUserDefaultError: Error {
-    case notFoundDefaults
-}
-
 class PersistentStoreUserDefaultsImpl: PersistentFavouritesStore {
     private let userDefaults: Atomic<UserDefaults>
 
@@ -13,9 +9,6 @@ class PersistentStoreUserDefaultsImpl: PersistentFavouritesStore {
     }
 
     init(userDefaults: UserDefaults) {
-        userDefaults.register(defaults: [
-            Keys.stocks.rawValue: [String: PersistentStoreStocksInfo](),
-        ])
         self.userDefaults = Atomic(userDefaults)
     }
 
@@ -32,36 +25,26 @@ class PersistentStoreUserDefaultsImpl: PersistentFavouritesStore {
     }
 
     func fetch(ids: [String]) -> AnyPublisher<[PersistentStoreStocksInfo], Error> {
-        guard
-            let items = self.userDefaults.value
-            .dictionary(forKey: Keys.stocks.rawValue) as? [String: PersistentStoreStocksInfo]
-        else {
-            return Fail(error: PersistentStoreUserDefaultError.notFoundDefaults)
+        do {
+            let items = try self.retrieve()
+            let filteredItems = items.filter { ids.contains($0.key) }
+            let sortedItems = filteredItems.values.sorted { $0.id < $1.id }
+            return Just(sortedItems)
+                .setFailureType(to: Error.self)
                 .eraseToAnyPublisher()
         }
-        let filteredItems = items.filter { ids.contains($0.key) }
-        let sortedItems = filteredItems.values.sorted { $0.id < $1.id }
-        return Just(sortedItems)
-            .setFailureType(to: Error.self)
-            .eraseToAnyPublisher()
+        catch {
+            return Fail(error: error)
+                .eraseToAnyPublisher()
+        }
     }
 
     private func mutate(_ transform: @escaping (inout [String: PersistentStoreStocksInfo]) -> Void)
         -> AnyPublisher<Bool, Error>
     {
         do {
-            // Start: Retrieve
-            guard
-                let data = self.userDefaults.value.object(forKey: Keys.stocks.rawValue) as? Data,
-                var items = try NSKeyedUnarchiver
-                .unarchiveTopLevelObjectWithData(data) as? [String: PersistentStoreStocksInfo]
-            else {
-                throw PersistentStoreUserDefaultError.notFoundDefaults
-            }
-            // End
-            // Start: Transform
+            var items = try self.retrieve()
             transform(&items)
-            // End
             // Start: Save
             let encodedData = try NSKeyedArchiver.archivedData(
                 withRootObject: items,
@@ -80,5 +63,17 @@ class PersistentStoreUserDefaultsImpl: PersistentFavouritesStore {
             return Fail(error: error)
                 .eraseToAnyPublisher()
         }
+    }
+
+    private func retrieve() throws -> [String: PersistentStoreStocksInfo] {
+        var items = [String: PersistentStoreStocksInfo]()
+        if
+            let data = self.userDefaults.value.object(forKey: Keys.stocks.rawValue) as? Data,
+            let cachedItems = try NSKeyedUnarchiver
+            .unarchiveTopLevelObjectWithData(data) as? [String: PersistentStoreStocksInfo]
+        {
+            items = cachedItems
+        }
+        return items
     }
 }
